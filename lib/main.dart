6d0cbad9b48af32e7a944e214af61c672edced07
Yusukei-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-12-26 15:00:12 +0900

    日本語化

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale("ja", "JP")
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
